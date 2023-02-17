f39fc43627f22f1fc50abc980dd28dc56d42db60
diegofdxz <113787026+diegofdxz@users.noreply.github.com>
2023-02-17 14:56:34 +0100

    Ejercicio 6

--- Ej6.java
+++ Ej6.java
@@ -10,6 +10,7 @@ Inicio
    valorIntroducido <- introducir("¿Cuál es la capital de Francia? ")
    MientrasQue valorIntroducido ≠ "París" o MAX_INTENTOS - numIntentos ≠ 0
          escribir("Respuesta incorrecta")
+        
          escribir("Solo quedan " & MAX_INTENTOS - numIntentos &
 " intento(s)")
          valorIntroducido <- introducir("¿Cuál es la capital de Francia? ")
@@ -23,7 +24,27 @@ Fin
 ¿Qué hace?
 
 Corrija este algoritmo para que logre lo que se podría esperar.
- */
-public class Ej6 {
 
-}
+Este algoritmo se encarga de dar 5 intentos al usuario para introducir correctamente la capital de Francia
+
+Variable numIntento : entero <- 1
+Variable valorIntroducido : texto
+Constante MAX_INTENTOS : entero <- 5
+Inicio
+   valorIntroducido <- introducir("¿Cuál es la capital de Francia? ")
+   MientrasQue valorIntroducido ≠ "París" o MAX_INTENTOS - numIntentos ≠ 0
+         escribir("Respuesta incorrecta")
+         numIntentos = numIntentos +1
+        
+         escribir("Solo quedan " & MAX_INTENTOS - numIntentos &
+" intento(s)")
+         valorIntroducido <- introducir("¿Cuál es la capital de Francia? ")
+   FMq
+   Si MAX_INTENTOS - numIntento ≠ 0 Entonces
+         escribir("Bravo")
+   Si no
+         escribir("Revise sus conocimientos en geografía")
+   FSi
+Fin
+ */
+
